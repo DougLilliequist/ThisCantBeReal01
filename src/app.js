985b4b97c59f1b25499fe04a84d5da4c79814fce
Douglas Lilliequist <douglaslilliequist@Douglass-MacBook-Pro.local>
2020-08-27 21:28:08 +0300

    preparing for publishing

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ export default class App {
 
     constructor() {
 
+        console.log('this can\'t be...real? ◕_◕');
+        console.log('douglas@adventureclub.io');
+        console.log('douglas.lilliequist@gmail.com');
+
         new OpticalFlow();
 
     }
